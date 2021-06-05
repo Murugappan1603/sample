4b7d83cf44398f09126a001267c8761bbaec1790
Murugappan1603 <85395230+Murugappan1603@users.noreply.github.com>
2021-06-05 16:58:06 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+num= int(input("enter the number between 1 and 7: "))
+if num==1:
+    print("its monday")
+elif num==2:
+    print("its tuesday")
+elif num==3:
+    print("its wednesday")
+elif num==4:
+    print("its thursday")
+elif num==5:
+    print("its friday")
+elif num==6:
+    print("its saturday")
+else:
+    print("its saturday")
